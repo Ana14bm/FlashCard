f02829484843f7b6ee5098a2989226f35d8a42b1
Ana14bm <barreto.martins@escola.pr.gov.br>
2025-08-14 15:32:02 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,59 @@
+criaCartao(
+    'Química',
+    'O que são soluções?',
+    'Soluções são misturas homogêneas onde uma substância (soluto) é dissolvida em outra(solvente).',
+)
+
+criaCartao(
+    'Química',
+    'Quais são os estados físicos da matéria?',
+    'Os estados físicos da matéria são: sólido, líquido, gasoso e plasma (embora plasma seja menos comum em condições normais).',
+)
+
+criaCartao(
+    'Química',
+    'O que é um ácido segundo a teoria de Arrhenius?',
+    'Ácidos são substâncias que, em solução aquosa, se ionizam e liberam íons H+.',
+)
+
+criaCartao(
+    'Geografia',
+    'O que é o fenômeno da inversão térmica?',
+    'É um fenômeno atmosférico natural em que uma camada de ar quente se sobrepõe a uma de ar frio, impedindo a dispersão de poluentes.',
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a diferença entre tempo e clima?',
+    'Tempo se refere às condições atmosféricas de um local em um momento específico, enquanto clima é a média dessas condições ao longo de um longo período.',
+)
+
+criaCartao(
+    'Geografia',
+    'O que são as placas tectônicas?',
+    'São grandes blocos rochosos que formam a crosta terrestre e se movem lentamente sobre o magma, causando terremotos e vulcanismos.',
+)
+
+criaCartao(
+    'Biologia',
+    'Qual a principal diferença entre células procarióticas e eucarióticas?',
+    'Células eucarióticas possuem um núcleo definido e organelas membranosas, enquanto as procarióticas não.',
+)
+
+criaCartao(
+    'Biologia',
+    'Qual a função da mitocôndria?',
+    'É a organela responsável pela respiração celular, produzindo a maior parte da energia (ATP) da célula.',
+)
+
+criaCartao(
+    'Biologia',
+    'O que é um gene?',
+    'É um segmento de DNA que contém a informação para a síntese de uma proteína ou uma molécula de RNA, determinando uma característica.',
+)
+
+
+
+
+    
+
